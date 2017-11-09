7745dd826463dff0608cc57e995e41c8867e8cff
tn841 <tn841@naver.com>
2017-11-09 11:11:55 +0900

    datatables tutorial에 다운로드 기능 추가

--- datatables_sqlalchemy/controller/serverside.py
+++ datatables_sqlalchemy/controller/serverside.py
@@ -1,12 +1,13 @@
 #-*- coding:utf-8 -*-
-from datatables import DataTables
+import datetime
+
+import xlsxwriter
 from datatables_blueprint import datatables_app
-from flask.globals import request
-from flask.helpers import make_response
+from datetime import datetime
+from flask.helpers import make_response, send_from_directory
 from flask.json import jsonify
 from flask.templating import render_template
 from datatables_sqlalchemy.DBManager import DBManager
-from datatables_sqlalchemy.model.Book import Book
 
 
 @datatables_app.route("/")
@@ -33,8 +34,10 @@ def get_table_data_func():
     conn = DBManager.conn
     cursor = conn.cursor()
     try:
-        cursor.callproc('select_all_books')
+        cursor.callproc('select_all_books') #book데이터를 가져오는 프로시저 호출
         data = cursor.fetchall()
+
+        #datatables lib에서 사용하는 json형식에 맞게 dictionary생성
         tabledata = {}
         tabledata["draw"] = 1
         tabledata["recordsTotal"] = len(data)
@@ -42,10 +45,50 @@ def get_table_data_func():
         print str(tabledata)
 
         #datatables 모듈을 이용해서 적절한 형식으로 데이터를 반환한다.
+        #sqlalchemy ORM을 활용할 수 있다.
     except Exception as e:
         tabledata = {"error" : e}
     finally:
         cursor.close()
-        return make_response(jsonify(tabledata))
+        return make_response(jsonify(tabledata))    #json형식으로 데이터를 반환한다.
+
+
+@datatables_app.route("/excel_download")
+def excel_donwload():
+    # Xlsxwrite
+    date_string = datetime.now().strftime('%Y%m%d%H%M%S')
+    workbook = xlsxwriter.Workbook('./resources/%s.xlsx' % date_string)
+    worksheet = workbook.add_worksheet('book_data')
+
+    s_row = 2
+    s_col = 1
+
+    # DB연결
+    conn = DBManager.conn
+    cursor = conn.cursor()
+
+    cursor.callproc('select_all_books')
+    data = cursor.fetchall()
+    cursor.close()
+
+    worksheet.write(s_row-1, 1, 'book id')
+    worksheet.write(s_row-1, 2, 'book name')
+    worksheet.write(s_row-1, 3, 'book price')
+    worksheet.write(s_row-1, 4, 'book type')
+
+    for row_data in data:
+        for col_data in row_data:
+            worksheet.write(s_row, s_col, col_data)
+            s_col += 1
+        s_col = 1
+        s_row += 1
+
+    worksheet.write(s_row, 2, 'Total')
+    worksheet.write(s_row, 3, '=sum(D3:D12)')
+    worksheet.write(s_row, 4, '=counta(E3:E12)')
+
+    workbook.close()
+
+    return send_from_directory('./resources', '%s.xlsx' % date_string, as_attachment=True)
 
 
